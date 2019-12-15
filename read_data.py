d3a1b22e1779f2c67a158af24aea0a836b659bde
arty <keith.elam.guru@gmail.com>
2019-12-15 11:53:29 -0500

    deleted some files

--- read_data.py
+++
@@ -1,9 +0,0 @@
-
-def read_dataset(filename):
-    print('Reading data from %s' % filename)
-    df = pd.read_csv(filename)
-    df.datetime = pd.to_datetime(df.datetime)   # change type from object to datetime
-    df = df.set_index('datetime')
-    df = df.sort_index()   # sort by datetime
-    print(df.shape)
-    return df
